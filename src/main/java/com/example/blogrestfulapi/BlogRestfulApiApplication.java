7b946b26c4efc989e813117031c2baef0055afd7
Jaime Rojas <jaime_rojas@claro.com.do>
2018-11-01 14:54:01 -0400

    Initial commit

---
+++ src/main/java/com/example/blogrestfulapi/BlogRestfulApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.blogrestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogRestfulApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BlogRestfulApiApplication.class, args);
+  }
+}
